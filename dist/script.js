e03c90ab3f57540e7d3402d495a4823cd2888f07
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-08-25 13:23:52 +0100

    commit

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,15 @@
-var url = "https://api.adviceslip.com/advice"
+const url = "https://api.adviceslip.com/advice";
+const adviceCont = document.querySelector(".adviceCont");
 
+const setAd = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      adviceCont.textContent = data.slip.advice;
+    })
+    .catch(() => {
+      adviceCont.textContent = "Error fetching message";
+    });
+};
 
-var getAd = async (url) => {
-    var randomAd = await fetch(url)
-    
-} 
-
-getAd(url)
+setAd();
